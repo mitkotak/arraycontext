f8038cdf3ad3757f287b1150dc69c0b956e92e03
mit kotak <mitak2@illinois.edu>
2021-11-12 20:54:36 -0600

    Updated design doc

--- arraycontext/impl/pycuda/src/design.py
+++ arraycontext/impl/pycuda/src/design.py
@@ -3,6 +3,14 @@ from typing import Iterable, Sequence
 
 
 class CudaGraphNode:
+    def get_dependenices(self) -> List[CudaGraphNode]:
+        ...
+        
+    def get_dependendent_nodes(self) -> List[CudaGraphNode]:
+        ...
+        
+
+class HostNodeParams:
     ...
 
 
@@ -17,92 +25,92 @@ class CudaGraph:
                         child_graph: CudaGraph) -> CudaGraphNode:
         ...
     
-    def create_graph(self): -> CudaGraph
+    def create_graph(self, flags: int): -> CudaGraph
         ...
 
-    def clone_graph(self,
-                    graph: CudaGraph) -> CudaGraph:
+    def clone_graph(self) -> CudaGraph:
         ...
     
 
-    def create_executable_graph(self,
-                                graph: CudaGraph): -> CudaExecutableGraph
+    def create_exec_graph(self,
+                          graph: CudaGraph): -> CudaExecutableGraph
         ...
     
-    def launch_executable_graph(self,
-                                graph_executable: CudaExecutableGraph,
-                                stream: CudaStream) -> None
+    def debug_dot_print(self,
+                        graph: CudaGraph,
+                        path : const char*
+                        flags : unsigned int): -> None
         ...
-
-    def remove_executable_graph(self,
-                                graph_executable: CudaExecutableGraph) -> None
-        ...
-
+        
     def add_dependencies(self,
                          from_: Sequence[CudaGraphNode],
                          to: Sequence[CudaGraphNode]) -> None:
         if len(from_) != len(to):
             raise ValueError(...)
         ...
-    
-    def get_dependencies(self,
-                         from_: Sequence[CudaGraphNode],
-                         to: Sequence[CudaGraphNode]) -> Iterable[CudaGraphNode]:
-        ...
-        
-    def remove_node(self,
-                    node: CudaGraphNode) -> None
-        ...
-    
+
     def add_empty_node(self,
                        dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
         ...
 
     def add_event_record_node(self,
-                              dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
+                              dependencies: Iterable[CudaGraphNode],
+                              event: Event) -> CudaGraphNode:
         ...
     
-    def get_event_event_record_node(self,
-                                    node: CudaGraphNode) -> *CudaEvent
+    def get_event_from_event_record_node(self,
+                                         node: CudaGraphNode
+                                         ) -> Event:
         ...
     
-    def set_event_event_record_node(self,
-                                    node: CudaGraphNode, event: *CudaEvent) -> None
+    def set_event_record_node_event(self,
+                                    node: CudaGraphNode,
+                                    event: Event) -> None
         ...
     
     def add_wait_node(self,
                       dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
         ...
     
-    def get_event_wait_node(self,
-                            node: CudaGraphNode) -> *CudaEvent
+    def get_event_from_event_wait_node(self,
+                                       node: CudaGraphNode
+                                       ) -> Event
         ...
     
-    def set_event_wait_node(self,
-                            node: CudaGraphNode, event: *CudaEvent) -> None
-        ...
-    
-    def add_external_semaphores_signal_node(self,
-                                            dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
-        ...
-    
-    def add_external_semaphores_wait_node(self,
-                                            dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
-        ...
-    
-    def add_host_node(self,
-                       dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
+    def set_event_wait_node_event(self,
+                            node: CudaGraphNode,
+                            event: Event) -> None
         ...
 
+
     def add_kernel_node(self,
-                       dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
+                       dependencies   : Iterable[CudaGraphNode],
+                       blockDim       : dim3,
+                       gridDim        : dim3,
+                       sharedMemBytes : unsigned int,
+                       func           : void) -> CudaGraphNode:
         ...
 
     def add_mem_alloc_node(self,
-                       dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
+                       dependencies   : Iterable[CudaGraphNode],
+                       accessDesCount : size_t,
+                       accessDescs    : cudaMemAccessDesc ,
+                       bytesize       : bytesize,
+                       dptr           : void,
+                       poolProps      : struct) -> CudaGraphNode:
         ...
     
     def add_memset_node(self,
-                       dependencies: Iterable[CudaGraphNode]) -> CudaGraphNode:
+                       dependencies: Iterable[CudaGraphNode],
+                       dst         : void,
+                       elementSize : unsigned int,
+                       height      : size_t,
+                       pitch       : size_t,
+                       value       : unsigned int,
+                       width       : size_t) -> CudaGraphNode:
+        ...
+
+class ExecGraph:
+    def launch(self, stream: Stream) -> None
         ...
     
