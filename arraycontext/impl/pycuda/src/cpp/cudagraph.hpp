c89dabb36306dfb7b6e4770495fd12ae197912eb
mit kotak <mitak2@illinois.edu>
2021-12-30 18:53:38 -0600

    First draft for C++ wrapper

--- arraycontext/impl/pycuda/src/cpp/cudagraph.hpp
+++ arraycontext/impl/pycuda/src/cpp/cudagraph.hpp
@@ -1,31 +1,119 @@
-#ifndef _AFJDFJSDFSD_PYCUDA_HEADER_SEEN_CURAND_HPP
-#define _AFJDFJSDFSD_PYCUDA_HEADER_SEEN_CURAND_HPP
-#endif
+#include <cuda.h>
+#include <stdint.h>
 
-#if CUDAPP_CUDA_VERSION >= 3020
-  #include <curand.h>
+namespace pycuda
+{
+  struct CudaGraph{
+    CUgraph_st graph;
 
-  #ifdef CUDAPP_TRACE_CUDA
-    #define CURAND_PRINT_ERROR_TRACE(NAME, CODE) \
-      if (CODE != CURAND_STATUS_SUCCESS) \
-        std::cerr << NAME << " failed with code " << CODE << std::endl;
-  #else
-    #define CURAND_PRINT_ERROR_TRACE(NAME, CODE) /*nothing*/
-  #endif
-
-  #define CURAND_CALL_GUARDED(NAME, ARGLIST) \
-    { \
-      CUDAPP_PRINT_CALL_TRACE(#NAME); \
-      curandStatus_t cu_status_code; \
-      cu_status_code = NAME ARGLIST; \
-      CURAND_PRINT_ERROR_TRACE(#NAME, cu_status_code); \
-      if (cu_status_code != CURAND_STATUS_SUCCESS) \
-        throw pycuda::error(#NAME, CUDA_SUCCESS);\
+    CudaGraph(unsigned int flags=0){ 
+      CUDAPP_CALL_GUARDED(cuGraphCreate, (&graph, flags)); 
+      }
+    
+    inline void graph_destroy(){
+      CUDAPP_CALL_GUARDED(cuGraphDestroy,(graph)); 
     }
-#else
-  #define CURAND_PRINT_ERROR_TRACE(NAME, CODE) /*nothing*/
-  #define CURAND_CALL_GUARDED(NAME, ARGLIST) /*nothing*/
-#endif
+
+    inline void node_destroy(CUgraphNode node){
+      CUDAPP_CALL_GUARDED(cuGraphDestroyNode,(node));
+    }
+    
+    inline CudaGraph graph_clone(){
+      CudaGraph graph_clone;
+      CUDAPP_CALL_GUARDED(cuGraphClone,(&graph_clone, graph));
+      return graph_clone;
+    }
+    
+    inline CUgraphNode get_root_nodes(unsigned int numNodes){
+      CUgraphNode nodes;
+      CUDAPP_CALL_GUARDED(cuGraphGetNodes, (graph, &nodes, &numNodes));
+      return nodes;
+    }
+
+    inline CUgraphNode get_root_nodes(unsigned int numRootNodes){
+      CUgraphNode root_nodes;
+      CUDAPP_CALL_GUARDED(cuGraphGetRootNodes, (graph, &root_nodes, &numRootNodes));
+      return root_nodes;
+    }
+
+    inline void add_dependencies(const CUgraphNode from_nodes, const CUgraphNode to_nodes, unsigned int numdependencies=0){
+      CUDAPP_CALL_GUARDED(cuGraphAddDependencies,(graph, &from_nodes, &to_nodes, numdependencies));
+    }
+
+    std::tuple <CudaGraphNode, CudaGraphNode> get_edges(unsigned int numdependencies=0){
+      const CUgraphNode from_nodes;
+      const CUgraphNode to_nodes;
+      CUDAPP_CALL_GUARDED(cuGraphGetEdges,(graph, &from_nodes, &to_nodes, numdependencies));
+      return make_tuple(from_nodes, to_nodes);
+    }
+
+    inline CUgraphNode get_dependencies(CUgraphNode node, unsigned int numdependencies=0){
+      CUgraphNode dependencies;
+      CUDAPP_CALL_GUARDED(cuGraphNodeGetDependencies, (node, &dependencies, &numdependencies));
+      return dependencies;
+    }
+
+    inline CUgraphNode get_dependent_nodes(CUgraphNode node, unsigned int numdependentnodes=0){
+      CUgraphNode dependent_nodes;
+      CUDAPP_CALL_GUARDED(cuGraphNodeGetDependentNodes, (node, &dependent_nodes, &numdependentnodes));
+      return dependent_nodes;
+    }
 
 
+    inline CUgraphNode add_child_graph_node(const CUgraphNode dependencies, unsigned int numdependencies=0, CudaGraph child_graph){
+      CUgraphNode child_node;
+      CUDAPP_CALL_GUARDED(cuGraphAddChildGraphNode,(&child_node, graph, &dependencies, numdependencies, child_graph));
+      return child_node;
+    }
 
+    inline CUgraphNode add_empty_node(const CUgraphNode *dependencies, unsigned int numdependencies=0){
+      CUgraphNode empty_node;
+      CUDAPP_CALL_GUARDED(cuGraphAddEmptyNode,(*empty_node, graph, dependencies, numdependencies));
+      return empty_node;
+    }
+
+    inline CUgraphNode add_event_record_node(Event event, const CUgraphNode dependencies, unsigned int numdependencies=0){
+      CUgraphNode event_record_node;
+      CUDAPP_CALL_GUARDED(cuGraphAddEventRecordNode,(&event_record_node, graph, &dependencies, numdependencies, event));
+      return event_record_node;
+    }
+
+    inline CUgraphNode add_event_wait_node(Event event, const CUgraphNode dependencies, unsigned int numdependencies=0){
+      CUgraphNode event_wait_node;
+      CUDAPP_CALL_GUARDED(cuGraphAddEventWaitNode,(&event_record_node, graph, &dependencies, numdependencies, event));
+      return event_wait_node;
+    }
+
+    inline Event get_event_from_event_record_node(CUgraphNode event_record_node){
+      CUevent event;
+      CUDAPP_CALL_GUARDED(cuGraphEventRecordNodeGetEvent,(&event_record_node, &event));
+      return event
+    }
+
+    inline Event get_event_from_event_wait_node(CUgraphNode event_wait_node){
+      CUevent event;
+      CUDAPP_CALL_GUARDED(cuGraphEventWaitNodeGetEvent,(event_wait_node, &event));
+      return event
+    }
+
+    inline void set_record_node_event(ExecGraph graph_exec, CUgraphNode event_record_node, CUevent event){
+      CUDAPP_CALL_GUARDED(cuGraphEventWaitNodeSetEvent,(graph_exec, event_record_node, event));
+    }
+
+    inline void set_wait_node_event(ExecGraph graph_exec, CUgraphNode event_wait_node, CUevent event){
+      CUDAPP_CALL_GUARDED(cuGraphEventWaitNodeSetEvent,(graph_exec, event_wait_node, event));
+    }
+
+    inline CUgraphNode add_host_node(const CUgraphNode dependencies, unsigned int numdependencies=0, const CUDA_HOST_NODE_PARAMS &nodeParams){
+      CUgraphNode host_node;
+      CUDAPP_CALL_GUARDED(cuGraphAddHostNode,(&host_node, graph, &dependencies, numdependencies, &nodeParams));
+      return host_node;
+    }
+
+
+  }
+
+}
+
+
+  
